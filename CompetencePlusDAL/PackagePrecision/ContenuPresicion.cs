d8910533cdc47a92d2855a3bac5639e450e6d7af
abdrzak-bakaly <abdrzakbakaly@gmail.com>
2015-01-01 12:02:36 +0000

    Class Competence

--- CompetencePlusDAL/PackagePrecision/ContenuPresicion.cs
+++ CompetencePlusDAL/PackagePrecision/ContenuPresicion.cs
@@ -5,7 +5,47 @@ using System.Text;
 
 namespace CompetencePlus.PackagePrecision
 {
-    class ContenuPresicion
+    public class ContenuPresicion
     {
+        string nom;
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        double duree;
+
+        public double Duree
+        {
+            get { return duree; }
+            set { duree = value; }
+        }
+        int order;
+
+        public int Order
+        {
+            get { return order; }
+            set { order = value; }
+        }
+        public ContenuPresicion() { }
+        public ContenuPresicion(string nom, string description, double duree, int order) 
+        {
+            this.Nom = nom;
+            this.Description = description;
+            this.Duree = duree;
+            this.Order = order;
+        }
+        public override string ToString()
+        {
+            return this.nom;
+        }
     }
 }
